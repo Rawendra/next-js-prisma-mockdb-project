00c67504367b7e58b56005a70b46617e77f08b93
rawendra <rawendra@gmail.com>
2023-12-07 19:09:19 +0530

    form add page css corrected

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -2,39 +2,37 @@ import React from "react";
 
 function SnippetCreatePage() {
   return (
-    <div className="container mx-auto pt-12 bg-blue-100">
+    <div className="container mx-auto pt-12 bg-green-100">
       <form>
-        <h3 className="font-bold-m3">Create a Snipppet </h3>
-        <div className="flex flex-col">
-          <div className="flex gap-4">
-            <label className="w-12" htmlFor="title">
-              Title
-            </label>
-            <input
-              name="title"
-              id="id"
-              className="border rounded p-2 w-full"
-            ></input>
-          </div>
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            Title
+          </label>
+          <input
+            name="title"
+            className="border rounded p-2 w-full"
+            id="title"
+          />
         </div>
 
-        <div className="flex flex-col gap-4">
-          <div className="flex gap-4">
-            <label className="w-12" htmlFor="code">
-              Code
-            </label>
-            <textarea
-              name="code"
-              id="code"
-              className="border rounded p-2 w-full"
-            ></textarea>
-          </div>
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
+            Code
+          </label>
+          <textarea
+            name="code"
+            className="border rounded p-2 w-full"
+            id="code"
+          />
         </div>
 
-        <button type="submit" className="rounded text-white p-2 bg-blue-700">
-          Submit
+        <button type="submit" className="rounded text-white p-2 bg-green-500">
+          Create
         </button>
-      </form>
+      </div>
+    </form>
     </div>
   );
 }
