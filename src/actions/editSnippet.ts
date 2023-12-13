e33af7b378c8c933e5ae4650977704cf99e7650b
rawendra <rawendra@gmail.com>
2023-12-14 02:47:44 +0530

    useformstate hook included

--- src/actions/editSnippet.ts
+++ src/actions/editSnippet.ts
@@ -2,7 +2,7 @@
 import { SnippetType } from "@/customTypes/Types";
 import { db } from "@/db";
 import { redirect } from "next/navigation";
-
+//import { revalidatePath } from "next/cache";
 export const handleEdit = async (props: SnippetType) => {
   const { id, code } = props;
 
@@ -13,3 +13,40 @@ export const handleEdit = async (props: SnippetType) => {
 
   redirect(`/snippets/${id}`);
 };
+
+export const deleteSnippet = async (id: number) => {
+  console.log("deleting id", id);
+  await db.snippet.delete({ where: { id } }); 
+  redirect("/");
+};
+
+export async function createSnippet(
+  formState: { message: String },
+  formData: FormData
+) {
+
+  const title = formData.get("title") 
+  const code = formData.get("code") 
+
+  if(typeof title !=='string' || title.length<3){
+    return {
+      message: "Title must be longer than 3 characters",
+    };
+  }
+
+  if(typeof code !=='string' || code.length<6){
+    return {
+      message: "Code must be longer than 6 characters",
+    };
+  }
+
+
+  const snippet = await db.snippet.create({
+    data: {
+      title,
+      code,
+    },
+  });
+  console.log("snippet", snippet);
+  redirect("/");
+}
--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -2,8 +2,8 @@ import { db } from "@/db";
 import React from "react";
 import { notFound } from "next/navigation";
 import { SnippetShowProps } from "@/customTypes/Types";
+import { deleteSnippet } from "@/actions/editSnippet";
 import Link from "next/link";
-
 
 async function Snippet(props: SnippetShowProps) {
   const {
@@ -15,18 +15,31 @@ async function Snippet(props: SnippetShowProps) {
   if (!snippet) {
     notFound();
   }
-  
+
+  const deleteSnippetAction = deleteSnippet.bind(null, parseInt(id));
   return (
     <div className="flex flex-col justify-center">
       <div className="flex m-4  items-center justify-between">
         <h1 className="text-xl font-bold">{snippet.title}</h1>
-        <div>
-          <Link href={`${id}/edit`} className="border p-2 rounded text-white bg-blue-500">
-            Edit
-          </Link>
-          <Link href='/' className="border p-2 rounded text-white bg-red-500">
-            Delete
-          </Link>
+        <div className="flex ">
+          <div>
+            {" "}
+            <Link
+              href={`${id}/edit`}
+              className="border p-2 rounded text-white bg-blue-500"
+            >
+              Edit
+            </Link>
+          </div>
+
+          <form action={deleteSnippetAction}>
+            <button
+              type="submit"
+              className="border p-2 rounded text-white bg-red-500"
+            >
+              Delete
+            </button>
+          </form>
         </div>
       </div>
       <pre className="p-3 border rounded bg-gray-200 border-gray-400">
